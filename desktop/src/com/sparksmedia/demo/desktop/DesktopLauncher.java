0001f171c800fb0e51017e82fd2bfb3fb5c15bf9
Cody <cody@localhost>
2016-08-25 17:51:31 -0600

    Render World

---
+++ desktop/src/com/sparksmedia/demo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sparksmedia.demo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sparksmedia.demo.Demo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Demo(), config);
+	}
+}
---
+++ ios/src/com/sparksmedia/demo/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.sparksmedia.demo;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.sparksmedia.demo.Demo;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new Demo(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
